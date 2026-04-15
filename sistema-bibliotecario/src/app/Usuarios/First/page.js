87b1960bd775a7f2d19e2539461b0d9521bc48d7
Gabrielle Ribeiro <gabriellerpadua@gmail.com>
2026-04-15 09:24:26 -0300

    Add user registration and login pages to the application

--- sistema-bibliotecario/src/app/Usuarios/First/page.js
+++ sistema-bibliotecario/src/app/Usuarios/First/page.js
@@ -6,12 +6,3 @@ export default function CadastroUsuarios(){
         </div>
     );
 }
-
-export default function LoginUsuario(){
-    return (
-        <div>
-            <h1>Login de Usuários</h1>
-            <p>Esta é a página de login de usuários da aplicação.</p>
-        </div>
-    );
-}
---
+++ sistema-bibliotecario/src/app/Usuarios/Second/page.js
@@ -0,0 +1,8 @@
+export default function LoginUsuario(){
+    return (
+        <div>
+            <h1>Login de Usuários</h1>
+            <p>Esta é a página de login de usuários da aplicação.</p>
+        </div>
+    );
+}
