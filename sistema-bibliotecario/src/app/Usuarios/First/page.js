b79da20143abfefc34ef800f341502166129f219
nathaliafdsousa <nathalia.fdsousa@gmail.com>
2026-04-27 15:23:00 -0300

    Remove unused user registration and login pages; add new pages for authentication

--- sistema-bibliotecario/src/app/Usuarios/First/page.js
+++
@@ -1,8 +0,0 @@
-export default function CadastroUsuarios(){
-    return (
-        <div>
-            <h1>Cadastro de Usuários</h1>
-            <p>Esta é a página de cadastro de usuários da aplicação.</p>
-        </div>
-    );
-}
--- sistema-bibliotecario/src/app/Usuarios/Second/page.js
+++
@@ -1,8 +0,0 @@
-export default function LoginUsuario(){
-    return (
-        <div>
-            <h1>Login de Usuários</h1>
-            <p>Esta é a página de login de usuários da aplicação.</p>
-        </div>
-    );
-}
